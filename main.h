af05df97fce2c30c6f2b7d21d1f4fc00635a2673
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-04-19 23:56:46 +0300

    done

--- main.h
+++ main.h
@@ -1,33 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int* printed);
-int print_unsignedToBinary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR (va_list arg);
-
-/**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- */
-typedef struct identifierStruct
-{
-char *indentifier;
-int (*printer)(va_list);
-} identifierStruct;
+int print_num(long int n);
+int print_string(char *str);
+int print_binary(unsigned int num);
+int print_unknown_spec(char c);
+int print_odh(char c, unsigned int num);
+int dec_to_oct(unsigned int num);
+int dec_to_hex(char c, unsigned int num);
+int print_S(char *);
+int print_reverse(char *s);
+int print_rot13(char *c);
+int print_pointer(void *p);
 
 #endif
